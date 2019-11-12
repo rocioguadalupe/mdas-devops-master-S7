27a1bf1f602745be5f46719f54e64682674c42a4
Rocio Guadalupe <rocio.k.guadalupe@gmail.com>
2019-11-13 00:18:38 +0100

    kubernetes

---
+++ pipeline-k8s.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+docker-compose rm -f  && \
+docker-compose up --build -d && \
+docker-compose push && \
+kubectl apply -f votingapp.yaml && \
+kubectl run mytests \
+	--generator=run-pod/v1 \
+	-- image=rocioguadalupe/votingapp-test \
+	-- env VOTINGAPP_HOST=myvotingapp.votingapp \
+	-- rm –attach –restart=Never && \
+echo GREEN  echo RED
